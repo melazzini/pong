abe759b8129a8a814fd2c0d0c0fb2162d8d4f022
francisco melazzini <francisco.melazzini@physics.msu.ru>
2023-08-24 13:50:08 +0300

    added the basic position and velocity to the paddle and added the testlist file

--- test/TestPaddle.cpp
+++ test/TestPaddle.cpp
@@ -1,19 +1,71 @@
 //
 // Created by francisco on 8/21/23.
 //
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include <glm/glm.hpp>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 using testing::Eq;
 
-struct Paddle
+class Paddle
 {
-    glm::vec2 position{};
+  public:
+    explicit Paddle(glm::vec2 position) : m_position{position},m_velocity{}
+    {
+    }
+
+    [[nodiscard]]glm::vec2 position() const
+    {
+        return m_position;
+    }
+
+    [[nodiscard]] glm::vec2 velocity()const
+    {
+        return m_velocity;
+    }
+
+    void setVelocity(glm::vec2 newVelocity)
+    {
+        m_velocity = newVelocity;
+    }
+
+    void setPosition(glm::vec2 newPosition)
+    {
+        m_position = newPosition;
+    }
+
+  private:
+    glm::vec2 m_position;
+    glm::vec2 m_velocity;
 };
 
-TEST(ThePaddle, StartsAtTopLeftPosition)
+struct ThePaddle:testing::Test
 {
-    Paddle paddle;
-    ASSERT_THAT(paddle.position,Eq(glm::vec2{}));
+    glm::vec2 initialPosition{};
+};
+
+TEST_F(ThePaddle, StartsAtTheInitialPosition)
+{
+    Paddle paddle{initialPosition};
+    ASSERT_THAT(paddle.position(), Eq(initialPosition));
+}
+
+TEST_F(ThePaddle, StartsAtRest)
+{
+    Paddle paddle{initialPosition};
+    ASSERT_THAT(paddle.velocity(), Eq(glm::vec2{}));
+}
+
+TEST_F(ThePaddle,CanSetItsVelocity)
+{
+    Paddle paddle{initialPosition};
+    paddle.setVelocity(glm::vec2{10,10});
+    ASSERT_THAT(paddle.velocity(),Eq(glm::vec2{10,10}));
+}
+
+TEST_F(ThePaddle,CanSetItsPosition)
+{
+    Paddle paddle{initialPosition};
+    paddle.setPosition(glm::vec2{10,10});
+    ASSERT_THAT(paddle.position(),Eq(glm::vec2{10,10}));
 }
