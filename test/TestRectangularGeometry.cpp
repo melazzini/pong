da9fc48a9e3ab03c2462db252ee7b0dfa232207b
francisco melazzini <francisco.melazzini@physics.msu.ru>
2023-08-25 21:04:58 +0300

    refactor the test for Rectangulargeometry

--- test/TestRectangularGeometry.cpp
+++ test/TestRectangularGeometry.cpp
@@ -1,27 +1,33 @@
 #include "RectangularGeometry.h"
+#include <glm/fwd.hpp>
 #include <gmock/gmock.h>
 
 using testing::Eq;
 using testing::Ne;
 
+static const int dummyInitialWidth{10};
+static const int dummyInitialHeight{10};
+
 TEST(ARectangularGeometry, StartsWithGivenWidthAndHeight)
 {
-    RectangularGeometry rectGeometry{10, 10};
-    ASSERT_THAT(rectGeometry.width(), Eq(10));
-    ASSERT_THAT(rectGeometry.height(), Eq(10));
+    RectangularGeometry rectGeometry{dummyInitialWidth, dummyInitialHeight};
+    ASSERT_THAT(rectGeometry.width(), Eq(dummyInitialWidth));
+    ASSERT_THAT(rectGeometry.height(), Eq(dummyInitialHeight));
 }
 struct AnExistingRectangularGeometry : testing::Test
 {
-    RectangularGeometry rectGeometry{0, -1};
+    RectangularGeometry rectGeometry{dummyInitialWidth, dummyInitialHeight};
+    static const int newDummyWidth{100};
+    static const int newDummyHeight{-10};
 };
 
 TEST_F(AnExistingRectangularGeometry, CanBeFullyResized)
 {
-    ASSERT_THAT(rectGeometry.width(), Ne(10));
-    ASSERT_THAT(rectGeometry.height(), Ne(11));
+    ASSERT_THAT(rectGeometry.width(), Ne(newDummyWidth));
+    ASSERT_THAT(rectGeometry.height(), Ne(newDummyHeight));
 
-    rectGeometry.resize(10, 11);
+    rectGeometry.resize(newDummyWidth, newDummyHeight);
 
-    ASSERT_THAT(rectGeometry.width(), Eq(10));
-    ASSERT_THAT(rectGeometry.height(), Eq(11));
+    ASSERT_THAT(rectGeometry.width(), Eq(newDummyWidth));
+    ASSERT_THAT(rectGeometry.height(), Eq(newDummyHeight));
 }
