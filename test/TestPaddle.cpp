c32c7f57049ca157afa819d7fad20e79e884ce88
francisco melazzini <francisco.melazzini@physics.msu.ru>
2023-08-25 15:50:45 +0300

    changed the build directory

--- test/TestPaddle.cpp
+++ test/TestPaddle.cpp
@@ -14,8 +14,8 @@ static const glm::vec2 paddleInitialPosition{};
 struct AnExistingPaddle : testing::Test
 {
     Paddle paddle{paddleInitialPosition};
-    glm::vec2 dummyPosition{10,-20};
-    glm::vec2 dummyVelocity{10,-20};
+    glm::vec2 dummyPosition{10, -20};
+    glm::vec2 dummyVelocity{10, -20};
 };
 
 TEST(APaddle, StartsAtTheGivenInitialPosition)
@@ -32,14 +32,14 @@ TEST(APaddle, StartsAtRest)
 
 TEST_F(AnExistingPaddle, CanChangeItsPosition)
 {
-    ASSERT_THAT(paddle.position(),Ne(dummyPosition));
+    ASSERT_THAT(paddle.position(), Ne(dummyPosition));
     paddle.setPosition(dummyPosition);
     ASSERT_THAT(paddle.position(), Eq(dummyPosition));
 }
 
 TEST_F(AnExistingPaddle, CanChangeItsVelocity)
 {
-    ASSERT_THAT(paddle.velocity(),Ne(dummyVelocity));
+    ASSERT_THAT(paddle.velocity(), Ne(dummyVelocity));
     paddle.setVelocity(dummyVelocity);
     ASSERT_THAT(paddle.velocity(), Eq(dummyVelocity));
 }
